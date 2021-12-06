9ee9811fd1031bf0a80b57348fd3a7e17c81c432
alexandra-scott <77768458+alexandra-scott@users.noreply.github.com>
2021-12-06 12:06:36 -0500

    Add files via upload

--- process_form.js
+++ process_form.js
@@ -7,10 +7,33 @@ var qs = require('querystring');
 var port = process.env.PORT || 3000;
 //var port = 8080;
 console.log("This goes to the console window")
-
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<h2> Search Results </h2>");
-    res.write("Success! This app is deployed online");
-    res.end();    
+    
+    if (req.url == "/"){
+        file="api-test.html";
+        fs.readFile(file, function(err, txt) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write("This is the home page <br>");
+            res.write(txt);
+            res.end();
+        });
+    }
+    else if (req.url == "/process")
+    {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("<h2> Search Results </h2>");
+        res.write("Success! This app is deployed online");
+        res.end(); 
+      });
+      
+      });
+
+      
+    }
+    else {
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write("Unknown page request");
+        res.end();
+    }
+    
 }).listen(port);
